a1a71718476d9add07c11551c22685b8fdc5009a
romainlob <lobelloromain@gmail.com>
2026-08-17 21:22:43 +0200

    Force HTTPS en production (assets/URLs derriere proxy Render)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
         $this->configureDefaults();
     }
 
